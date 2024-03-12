6085764189cb8b1db7870ca1e6b58e5686b04e31
Fréderic LOUI <frederic.loui@renater.fr>
2024-03-12 17:05:16 +0100

    wrap (event) using (span) as EnvFilter is considering only on_new_span

--- tracing-async-server-envfilter/src/main.rs
+++ tracing-async-server-envfilter/src/main.rs
@@ -6,7 +6,7 @@ use tokio::{
     sync::mpsc,
     sync::mpsc::Sender,
 };
-use tracing::{info, instrument, level_filters::LevelFilter, span, trace, Instrument, Level};
+use tracing::{info, instrument, level_filters::LevelFilter, span, trace, Level};
 use tracing_subscriber::{filter, fmt, prelude::*, reload};
 
 #[tokio::main]
@@ -17,7 +17,7 @@ async fn main() -> Result<(), Error> {
     // Create a filter for INFO level
     let info_filter = filter::EnvFilter::builder()
         .with_default_directive(LevelFilter::INFO.into())
-        .parse_lossy("protocol[bgp]");
+        .parse_lossy("");
 
     // wrap info_filter using reload::layer
     let (info_filter, reload_handle) = reload::Layer::new(info_filter);
@@ -30,17 +30,19 @@ async fn main() -> Result<(), Error> {
     let task_number = 3;
     let mut tasks = Vec::with_capacity(task_number);
 
-    let ospf_span = span!(target: "protocol", Level::TRACE, "ospf");
-    tasks.push(tokio::spawn(ospf(1).instrument(ospf_span)));
+    // let ospf_span = span!(target: "protocol", Level::TRACE, "ospf");
+    // tasks.push(tokio::spawn(ospf(1).instrument(ospf_span)));
+    tasks.push(tokio::spawn(ospf(1)));
 
-    let bgp_span = span!(target: "protocol", Level::TRACE, "bgp");
-    tasks.push(tokio::spawn(bgp(2).instrument(bgp_span)));
+    // let bgp_span = span!(target: "protocol", Level::TRACE, "bgp");
+    // tasks.push(tokio::spawn(bgp(2).instrument(bgp_span)));
+    tasks.push(tokio::spawn(bgp(2)));
 
-    let socket_server_span =
-        span!(target: "log_remote_control", Level::TRACE, "log_remote_control");
-    tasks.push(tokio::spawn(
-        socket_server(tx).instrument(socket_server_span),
-    ));
+    // let socket_server_span =
+    //     span!(target: "log_remote_control", Level::TRACE, "log_remote_control");
+    // tasks.push(tokio::spawn(
+    //     socket_server(tx).instrument(socket_server_span),
+    tasks.push(tokio::spawn(socket_server(tx)));
 
     let mut outputs = Vec::with_capacity(tasks.len());
     while let Some(message) = rx.recv().await {
@@ -49,16 +51,29 @@ async fn main() -> Result<(), Error> {
         if message == 1 {
             println!("Setting LOG to protocol[ospf] ...");
             reload_handle.modify(|filter| {
+                let t = "protocol";
+                let s = "ospf";
+                let d_ospf = format!("{}[{}{{pid={}}}]", t, s, message);
                 *filter = filter::EnvFilter::builder()
                     .with_default_directive(LevelFilter::TRACE.into())
-                    .parse_lossy("protocol[ospf]")
+                    .parse_lossy(d_ospf)
             })?;
-        } else {
+        } else if message == 2 {
             println!("Setting LOG back to protocol[bgp] ...");
             reload_handle.modify(|filter| {
+                let t = "protocol";
+                let s = "bgp";
+                let d_bgp = format!("{}[{}{{pid={}}}]", t, s, message);
                 *filter = filter::EnvFilter::builder()
                     .with_default_directive(LevelFilter::TRACE.into())
-                    .parse_lossy("protocol[bgp]")
+                    .parse_lossy(d_bgp)
+            })?;
+        } else {
+            println!("Re-initialize LOG ...");
+            reload_handle.modify(|filter| {
+                *filter = filter::EnvFilter::builder()
+                    .with_default_directive(LevelFilter::INFO.into())
+                    .parse_lossy("")
             })?;
         }
     }
@@ -72,7 +87,6 @@ async fn main() -> Result<(), Error> {
     Ok(())
 }
 
-#[instrument]
 async fn socket_server(tx: Sender<u32>) {
     let listener = TcpListener::bind("localhost:6666").await.unwrap();
     info!(name: "socket_server", target="log_remote_control","WAITING INCOMING CONNECTION ...");
@@ -117,33 +131,49 @@ async fn socket_server(tx: Sender<u32>) {
     }
 }
 
-#[instrument(target = "protocol", name = "ospf")]
 async fn ospf(pid: usize) {
     let mut counter: u64 = 0;
-    info!(name: "ospf", target:"protocol", pid, "ENTERING OSPF ...");
+    let ospf_span = span!(target: "protocol", Level::TRACE,"ospf",pid);
+    ospf_span.in_scope(|| {
+        trace!("Entering OSPF...");
+    });
     loop {
-        trace!(name: "ospf", target: "protocol", pid, "This an OSPF message");
+        let ospf_span = span!(target: "protocol", Level::TRACE,"ospf",pid);
+        ospf_span.in_scope(|| {
+            trace!("This an OSPF message");
+        });
         tokio::time::sleep(Duration::from_secs(4)).await;
         counter += 1;
         if counter == 20 {
             break;
         }
     }
-    info!(name: "ospf", target:"protocol", pid, "EXITING OSPF ...");
+    let ospf_span = span!(target: "protocol", Level::TRACE,"ospf",pid);
+    ospf_span.in_scope(|| {
+        trace!("Exiting OSPF...");
+    });
 }
 
-#[instrument(target = "protocol", name = "bgp")]
 async fn bgp(pid: usize) {
     let mut counter: u64 = 0;
-    info!(name: "bgp", target:"protocol",pid, "ENTERING BGP ...");
+    let bgp_span = span!(target: "protocol", Level::TRACE,"bgp",pid);
+    bgp_span.in_scope(|| {
+        trace!("Entering BGP ...");
+    });
     loop {
-        trace!(name: "bgp", target: "protocol", pid, "This a BGP message");
+        let bgp_span = span!(target: "protocol", Level::TRACE,"bgp",pid);
+        bgp_span.in_scope(|| {
+            trace!("This a BGP message");
+        });
         tokio::time::sleep(Duration::from_secs(2)).await;
         counter += 1;
         if counter == 20 {
             break;
         }
     }
-    info!(name: "bgp", target: "protocol", pid,"EXITING BGP ...");
+    let bgp_span = span!(target: "protocol", Level::TRACE,"bgp",pid);
+    bgp_span.in_scope(|| {
+        trace!("Exiting BGP ...");
+    });
 }
 
